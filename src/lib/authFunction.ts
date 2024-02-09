2f0be4a82a38a6533e80a572ff769cb35bba6543
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-09 11:36:48 +0530

    feat: Implement login and sign-in with Google using Firebase

---
+++ src/lib/authFunction.ts
@@ -0,0 +1,19 @@
+import { auth } from "@/firebase/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  try {
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.error("Error signing in with Google", error);
+  }
+}
+
+export async function signOut() {
+  try {
+    return auth.signOut();
+  } catch (error) {
+    console.error("Error signing out with Google", error);
+  }
+}
