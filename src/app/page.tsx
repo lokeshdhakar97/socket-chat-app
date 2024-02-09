16fb60a8ebc1fd81672f4895b0c81b9b7f26d4b9
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-09 12:07:21 +0530

    feat: Connect project to MongoDB for database integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import ChatBoard from "@/components/ChatBoard";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Click Me</Button>
+      <ChatBoard />
     </main>
   );
 }
